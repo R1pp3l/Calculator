e822a3de25006c791bac8591cc02bbb652c5939c
R1pp3l <victorcabralweb@outlook.com>
2023-02-08 22:13:34 +0800

    Temporarily disabled JS code

---
+++ script.js
@@ -0,0 +1,43 @@
+/*let addOperator = function (a,b) {
+    console.log(a + b)
+}
+
+let subtractOperator = function (a,b) {
+    console.log(a - b)
+}
+
+let multiplyOperator = function (a,b) {
+    console.log(a * b)
+}
+
+let divideOperator = function (a,b) {
+    console.log(a / b)
+}
+
+let numOne = 5;
+let numTwo = 5;
+
+let operate = function () {
+    let question = prompt("Enter operator: ");
+    switch (question) {
+
+        case 'add':
+            addOperator(numOne, numTwo);
+            break;
+
+        case 'subtract':
+            subtractOperator(numOne, numTwo);
+            break;
+
+        case 'multiply':
+            multiplyOperator(numOne, numTwo);
+            break;
+
+        case 'divide':
+            divideOperator(numTwo, numOne);
+            break;
+
+    }
+}
+
+operate() */
