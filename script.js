44bc4a90772615cbc36159811814d66745c03309
R1pp3l <victorcabralweb@outlook.com>
2023-02-09 15:14:59 +0800

    Proper functions and syntax for calculating the value of 'display'

--- script.js
+++ script.js
@@ -15,3 +15,56 @@ clears.addEventListener('click', function() {
     display.innerText = "";
 })
 
+equal.addEventListener('click', function() {
+    const expression = display.innerText;
+    display.innerText = operate(expression);
+})
+
+function operate(expression) {
+    let result = 0;
+    let currentValue ='';
+    let currentOperator = '+';
+
+    for (let i = 0; i < expression.length; i++) {
+        const char = expression[i];
+
+        if (!isNaN(char) || char === '.') {
+            currentValue += char;
+        } else {
+            switch (currentOperator ) {
+                case '+' :
+                    result += parseFloat(currentValue);
+                    break;
+                case '-' :
+                    result -= parseFloat(currentValue);
+                    break;
+                case 'X' :
+                    result *= parseFloat(currentValue);
+                    break;
+                case '/' :
+                    result /= parseFloat(currentValue);
+                    break;
+            }
+            currentValue = '';
+            currentOperator = char;
+        }
+    }
+
+    switch (currentOperator) {
+        case '+' :
+            result += parseFloat(currentValue);
+            break;
+        case '-' :
+            result -= parseFloat(currentValue);
+            break;
+        case 'X' :
+            result *= parseFloat(currentValue);
+            break;
+        case '/' :
+            result /= parseFloat(currentValue);
+            break;
+    }
+
+    return result;
+
+}
